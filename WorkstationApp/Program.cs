84be26fa19bc7af82f420c5a8b5d1cd1a5869480
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-05-07 21:58:27 +0600

    Client Xamarin + Server Windows console

--- WorkstationApp/Program.cs
+++ WorkstationApp/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkstationApp
 {
     class Program
     {
+        public static TcpClient client;
+        private static TcpListener listener;
+        private static string ipString;
+
+
         static void Main(string[] args)
         {
+            //ПОЛУЧАЕМ адресс локального компьютера
+            IPAddress[] localIp = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress address in localIp)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork) 
+                {
+                    ipString = address.ToString();
+                }
+            }
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipString), 1234);
+            listener = new TcpListener(ep);
+            listener.Start();
+
+            Console.WriteLine(@"
+            ================================================================
+            Start listening requests at: {0}:{1}
+            ================================================================",
+            ep.Address, ep.Port);
+            client = listener.AcceptTcpClient();
+            Console.WriteLine("Connected to client!" + " \n");
+
+            try
+            {
+                const int bytesize = 1024 * 1024;
+                byte[] buffer = new byte[bytesize];
+                string x = client.GetStream().Read(buffer, 0, bytesize).ToString();
+
+                var data = ASCIIEncoding.ASCII.GetString(buffer);
+                if (data.ToUpper().Contains("SLP2")) 
+                {
+                    Console.WriteLine("PC is going to Sleep Mode!" + "\n");
+                    Sleep();
+                }
+            }
+            catch (Exception exc)
+            {
+                client.Dispose();
+                client.Close();
+                
+            }
+
+            void Sleep()
+            {
+                Console.WriteLine("Sleep!!");
+            }
+
         }
+        
     }
 }
---
+++ XamarinFormsClient/XamarinFormsClient/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Sockets;
+
+namespace XamarinFormsClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Connect_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                await client.ConnectAsync(IPAddress.Text, Convert.ToInt32(Port.Text));
+                if (client.Connected)
+                {
+                    Connection.Instance.client = client;
+                    Application.Current.MainPage = new NavigationPage(new OperationsPage());
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "" + ex.ToString(), "Ok");
+            }
+        }
+    }
+}
